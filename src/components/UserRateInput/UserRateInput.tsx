ad31a76a3109208699008fcd3ae8f7027f1b256a
Roman Kolmakov <smtexx@hotmail.com>
2023-05-08 14:37:01 +0200

    init: create component

---
+++ src/components/UserRateInput/UserRateInput.tsx
@@ -0,0 +1,35 @@
+import { setClass } from '../../lib/helpers';
+import Selector from '../Selector/Selector';
+import s from './UserRateInput.module.scss';
+
+export default function UserRateInput() {
+  return (
+    <form className={s.wrapper}>
+      <h3 className={s.title}>Добавить курс</h3>
+      <div className={s.fieldWrapper}>
+        <label htmlFor="fromCurrency">Начальная валюта:</label>
+        <Selector />
+      </div>
+      <div className={s.fieldWrapper}>
+        <label htmlFor="fromCurrency">Конечная валюта:</label>
+        <Selector />
+      </div>
+      <div className={s.fieldWrapper}>
+        <label htmlFor="newUserRate">По курсу:</label>
+        <input
+          className={setClass([['cm-input'], [s.rateValue]])}
+          type="number"
+          id="newUserRate"
+          value={0}
+        />
+      </div>
+
+      <button
+        className={setClass([['cm-input'], [s.addButton]])}
+        type="button"
+      >
+        Сохранить
+      </button>
+    </form>
+  );
+}
