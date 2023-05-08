d66236c6c201d037006c664d973b7db45c5c7331
Roman Kolmakov <smtexx@hotmail.com>
2023-05-08 15:33:37 +0200

    feat: add direction icon and UserRatesAded

--- src/components/UserRatesBlock/UserRatesBlock.tsx
+++ src/components/UserRatesBlock/UserRatesBlock.tsx
@@ -1,10 +1,16 @@
 import CustomBlock from '../CustomBlock/CustomBlock';
 import UserRateInput from '../UserRateInput/UserRateInput';
+import UserRatesAdded from '../UserRatesAdded/UserRatesAdded';
+import { BiChevronsDown } from 'react-icons/bi';
 
 export default function UserRatesBlock() {
   return (
     <CustomBlock title="Пользовательские курсы">
       <UserRateInput />
+      <div className="cm-direction-icon">
+        <BiChevronsDown />
+      </div>
+      <UserRatesAdded />
     </CustomBlock>
   );
 }
