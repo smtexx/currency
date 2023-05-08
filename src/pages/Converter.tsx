8a095607a1d377138e40c34c183769510481cc21
Roman Kolmakov <smtexx@hotmail.com>
2023-05-08 12:57:48 +0200

    feat: add Exchange component

--- src/pages/Converter.tsx
+++ src/pages/Converter.tsx
@@ -1,9 +1,11 @@
+import Exchange from '../components/Exchange/Exchange';
 import Updated from '../components/Updated/Updated';
 
 export default function Converter() {
   return (
     <>
       <Updated />
+      <Exchange />
     </>
   );
 }
