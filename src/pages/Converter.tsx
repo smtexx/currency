fc670876d1bba3e5d855f1c27be8404eac3aa073
Roman Kolmakov <smtexx@hotmail.com>
2023-05-08 14:05:06 +0200

    feat: add UserRatesBlock

--- src/pages/Converter.tsx
+++ src/pages/Converter.tsx
@@ -1,11 +1,13 @@
 import ExchangeBlock from '../components/ExchangeBlock/ExchangeBlock';
 import Updated from '../components/Updated/Updated';
+import UserRatesBlock from '../components/UserRatesBlock/UserRatesBlock';
 
 export default function Converter() {
   return (
     <>
       <Updated />
       <ExchangeBlock />
+      <UserRatesBlock />
     </>
   );
 }
