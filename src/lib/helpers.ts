9030e6ebe1b252624f0a8a5d93968b6ca543395e
Roman Kolmakov <smtexx@hotmail.com>
2023-05-14 14:09:57 +0200

    feat: setClass throws error if class name is undefined

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,9 +1,19 @@
-type AloneClass = [string];
-type OptionalClass = [string, boolean];
-type TernaryClass = [string, string, boolean];
+type ClassName = string | undefined;
+type AloneClass = [ClassName];
+type OptionalClass = [ClassName, boolean];
+type TernaryClass = [ClassName, ClassName, boolean];
 type ClassConfig = AloneClass | OptionalClass | TernaryClass;
 
 export function setClass(config: ClassConfig[]): string {
+  // Throw error if one of arguments is undefined
+  config.forEach((className) => {
+    if (className.includes(undefined)) {
+      throw new Error(
+        `SetClass: one of arguments in config is undefined`
+      );
+    }
+  });
+
   // eslint-disable-next-line array-callback-return
   const newClassName = config.reduce((classPart, configPart) => {
     switch (configPart.length) {
