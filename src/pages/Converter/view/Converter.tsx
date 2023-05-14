0de4ba6b62b0ce2f36878c7e8ab507c61cf35fcb
Roman Kolmakov <smtexx@hotmail.com>
2023-05-14 13:43:04 +0200

    refc: move Converter screen into pages/view

---
+++ src/pages/Converter/view/Converter.tsx
@@ -0,0 +1,17 @@
+import AddedRatesBlock from '../../../components/AddedRatesBlock/AddedRatesBlock';
+import ExchangeBlock from '../../../components/ExchangeBlock/ExchangeBlock';
+import Updated from '../../../components/Updated/Updated';
+import UserRatesBlock from '../../../components/UserRatesBlock/UserRatesBlock';
+
+export default function Converter() {
+  return (
+    <>
+      <div className="cm-hide-on-tablet-m">
+        <Updated />
+      </div>
+      <ExchangeBlock />
+      <UserRatesBlock />
+      <AddedRatesBlock />
+    </>
+  );
+}
