7f40edcc5c934dded9596f0b4cebec6160e65c5a
Roman Kolmakov <smtexx@hotmail.com>
2023-05-17 19:25:16 +0200

    test: testing clearNumericInput, createStateCopy, toFixedString functions

--- src/lib/helpers.test.ts
+++ src/lib/helpers.test.ts
@@ -1,21 +1,88 @@
-import { setClass } from './helpers';
+import {
+  clearNumericInput,
+  createStateCopy,
+  safeParseFloat,
+  setClass,
+  toFixedString,
+} from './helpers';
 
-it('setClass helper returns right class name', () => {
-  const one = 'classOne';
-  const two = 'classTwo';
-  const three = 'classThree';
-  expect(setClass([[one]])).toBe(one);
-  expect(setClass([[one, true]])).toBe(one);
-  expect(setClass([[one, false]])).toBe('');
-  expect(setClass([[one], [two]])).toBe(`${one} ${two}`);
-  expect(setClass([[one], [two, true]])).toBe(`${one} ${two}`);
-  expect(setClass([[one], [two, false]])).toBe(one);
-  expect(setClass([[one], [two, three, true]])).toBe(`${one} ${two}`);
-  expect(setClass([[one], [two, three, false]])).toBe(
-    `${one} ${three}`
-  );
+describe('function setClass:', () => {
+  it('Returns right class name', () => {
+    const one = 'classOne';
+    const two = 'classTwo';
+    const three = 'classThree';
+    expect(setClass([[one]])).toBe(one);
+    expect(setClass([[one, true]])).toBe(one);
+    expect(setClass([[one, false]])).toBe('');
+    expect(setClass([[one], [two]])).toBe(`${one} ${two}`);
+    expect(setClass([[one], [two, true]])).toBe(`${one} ${two}`);
+    expect(setClass([[one], [two, false]])).toBe(one);
+    expect(setClass([[one], [two, three, true]])).toBe(
+      `${one} ${two}`
+    );
+    expect(setClass([[one], [two, three, false]])).toBe(
+      `${one} ${three}`
+    );
+  });
+
+  it('Throws error if class name is undefined', () => {
+    expect(() => setClass([[undefined]])).toThrow();
+  });
 });
 
-it('setClass throws error if class name is undefined', () => {
-  expect(() => setClass([[undefined]])).toThrow();
+describe('function createStateCopy:', () => {
+  it('Returns correct copy of state', () => {
+    const state = {
+      argOne: 5,
+      argTwo: 'str',
+      argThree: [1, 4, 6],
+    };
+    expect(createStateCopy(state)).toEqual(state);
+    expect(createStateCopy(state) === state).toBe(false);
+  });
+});
+
+describe('function clearNumericInput:', () => {
+  it('Returns correct string', () => {
+    expect(clearNumericInput('45fgjj678')).toBe('45678');
+    expect(clearNumericInput('fgjj678')).toBe('678');
+    expect(clearNumericInput('45fgjj')).toBe('45');
+    expect(clearNumericInput('456,56d')).toBe('456.56');
+    expect(clearNumericInput('00456,56')).toBe('456.56');
+    expect(clearNumericInput('00.456,56')).toBe('0.45656');
+    expect(clearNumericInput('0.0.4,56,5.6')).toBe('0.045656');
+    expect(clearNumericInput('rgea-=')).toBe('0');
+    expect(clearNumericInput('.')).toBe('0.');
+    expect(clearNumericInput('')).toBe('0');
+    expect(clearNumericInput('jgfg.klh8ghj')).toBe('0.8');
+    expect(clearNumericInput('jgfg.klhghj')).toBe('0.');
+    expect(clearNumericInput('456')).toBe('456');
+    expect(clearNumericInput('0.657')).toBe('0.657');
+    expect(clearNumericInput('.567')).toBe('0.567');
+    expect(
+      clearNumericInput(`dfdhf
+    .sgss7`)
+    ).toBe('0.7');
+  });
+});
+
+describe('function safeParseFloat:', () => {
+  it('Returns correct value', () => {
+    expect(safeParseFloat('0.543')).toBe(0.543);
+    expect(safeParseFloat('543')).toBe(543);
+    expect(safeParseFloat('543.56')).toBe(543.56);
+    expect(safeParseFloat('543,56')).toBe(0);
+    expect(safeParseFloat('x543,56')).toBe(0);
+    expect(safeParseFloat('hello')).toBe(0);
+  });
+});
+
+describe('function toFixedString:', () => {
+  it('Returns correct value', () => {
+    expect(toFixedString(456)).toBe('456');
+    expect(toFixedString(456.5)).toBe('456.5');
+    expect(toFixedString(456.05)).toBe('456.05');
+    expect(toFixedString(456.0567)).toBe('456.06');
+    expect(toFixedString(456.0)).toBe('456');
+  });
 });
