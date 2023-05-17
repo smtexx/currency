66390c748ae0e12ea3e690198c00defbb5f1a961
Roman Kolmakov <smtexx@hotmail.com>
2023-05-17 19:24:50 +0200

    feat: create clearNumericInput, createStateCopy, toFixedString functions

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -4,6 +4,7 @@ type OptionalClass = [ClassName, boolean];
 type TernaryClass = [ClassName, ClassName, boolean];
 type ClassConfig = AloneClass | OptionalClass | TernaryClass;
 
+// Function calculates class name based on config argument
 export function setClass(config: ClassConfig[]): string {
   // Throw error if one of arguments is undefined
   config.forEach((className) => {
@@ -31,4 +32,55 @@ export function setClass(config: ClassConfig[]): string {
   }, '');
 
   return newClassName.trim();
+}
+
+// Creates object copy using JSON object
+export function createStateCopy<State>(state: State): State {
+  return JSON.parse(JSON.stringify(state));
+}
+
+// Formats string before parsing float number,
+// trailing zeros are NOT truncated
+export function clearNumericInput(value: string): string {
+  if (value === '') {
+    return '0';
+  }
+  if (value === '.') {
+    return '0.';
+  }
+
+  let firstDot = true;
+  const cleared = value
+    // Remove all non numeric symbols, except dot and comma
+    .replace(/[^\d.,]/g, '')
+    // Replace comma to dot, if it exists
+    .replace(/,/g, '.')
+    // Delete all dots except the first
+    .replace(/\./g, () => {
+      if (firstDot) {
+        firstDot = false;
+        return '.';
+      } else {
+        return '';
+      }
+    })
+    // Delete leading zeros before numbers
+    .replace(/^0+(?=[1-9])/, '')
+    // Replace repeated leading zeros before dot to single
+    .replace(/^0{2,}(?=\.)/, '0')
+    // Add 0 if string starts with dot
+    .replace(/^\./, '0.');
+
+  // If all symbols deleted, return 0
+  if (cleared.length === 0) {
+    return '0';
+  }
+
+  return cleared;
+}
+
+// Converts a number type argument to a string type with
+// an accuracy of 2 decimal places. Trailing zeros are truncated.
+export function toFixedString(value: number): string {
+  return value.toFixed(2).replace(/\.?0*$/, '');
 }
