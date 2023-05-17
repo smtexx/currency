f681535f656dc90acfd6de438fb2f6fb63b63bac
Roman Kolmakov <smtexx@hotmail.com>
2023-05-17 19:29:01 +0200

    test: testing getUserRateHash, recalculateCurrencies functions

---
+++ src/pages/Converter/controller/dataProcessing.test.ts
@@ -0,0 +1,77 @@
+import { ConverterState, Rates } from '../../../types';
+import {
+  getUserRateHash,
+  recalculateCurrencies,
+} from './dataProcessing';
+
+describe('function getUserRateHash:', () => {
+  it('Returns correct hash', () => {
+    expect(getUserRateHash(['EUR', 'USD', 999])).toBe('EUR_USD');
+    expect(getUserRateHash(['USD', 'EUR', 999])).toBe('EUR_USD');
+    expect(getUserRateHash(['RUB', 'RSD', 999])).toBe('RSD_RUB');
+  });
+});
+
+describe('function recalculateCurrencies:', () => {
+  it('Correct recalculates CurrencyIO blocks by passed index', () => {
+    const state: ConverterState = {
+      updated: null,
+      currencyIO: [
+        { currency: 'RSD', value: '500' },
+        { currency: 'EUR', value: '1' },
+        { currency: 'USD', value: '345' },
+      ],
+      rates: { EUR: 0.9218, RSD: 107.8928, USD: 1 } as Rates,
+      userRates: {},
+    };
+    // Recalculate value by index 1
+    recalculateCurrencies(state, 1);
+    expect(state.currencyIO[0].value).toBe('117.05');
+    expect(state.currencyIO[1].value).toBe('1');
+    expect(state.currencyIO[2].value).toBe('1.08');
+
+    state.currencyIO[0].value = '999';
+    state.currencyIO[1].value = '999';
+    state.currencyIO[2].value = '1.5';
+
+    // Recalculate value by index 2
+    recalculateCurrencies(state, 2);
+    expect(state.currencyIO[0].value).toBe('161.84');
+    expect(state.currencyIO[1].value).toBe('1.38');
+    expect(state.currencyIO[2].value).toBe('1.5');
+  });
+
+  it('Correct recalculates CurrencyIO blocks with user rate', () => {
+    const state: ConverterState = {
+      updated: null,
+      currencyIO: [
+        { currency: 'RSD', value: '999' },
+        { currency: 'EUR', value: '1' },
+        { currency: 'USD', value: '999' },
+      ],
+      rates: { EUR: 0.9218, RSD: 107.8928, USD: 1 } as Rates,
+      userRates: { EUR_RSD: ['EUR', 'RSD', 117.5] },
+    };
+    recalculateCurrencies(state, 1);
+    expect(state.currencyIO[0].value).toBe('117.5');
+    expect(state.currencyIO[1].value).toBe('1');
+    expect(state.currencyIO[2].value).toBe('1.08');
+  });
+
+  it('Correct recalculates CurrencyIO blocks with reverce user rate', () => {
+    const state: ConverterState = {
+      updated: null,
+      currencyIO: [
+        { currency: 'RSD', value: '235' },
+        { currency: 'EUR', value: '999' },
+        { currency: 'USD', value: '999' },
+      ],
+      rates: { EUR: 0.9218, RSD: 107.8928, USD: 1 } as Rates,
+      userRates: { EUR_RSD: ['EUR', 'RSD', 117.5] },
+    };
+    recalculateCurrencies(state, 0);
+    expect(state.currencyIO[0].value).toBe('235');
+    expect(state.currencyIO[1].value).toBe('2');
+    expect(state.currencyIO[2].value).toBe('2.18');
+  });
+});
