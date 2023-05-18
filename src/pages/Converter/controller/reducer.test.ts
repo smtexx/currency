a661c096c94c35a3238fdf5c85865d552bfb6b79
Roman Kolmakov <smtexx@hotmail.com>
2023-05-18 16:40:15 +0200

    feat: create and test CHANGE_CURRENCY part of reducer

--- src/pages/Converter/controller/reducer.test.ts
+++ src/pages/Converter/controller/reducer.test.ts
@@ -9,18 +9,44 @@ describe('function reducer:', () => {
       { currency: 'RUB', value: '999' },
       { currency: 'USD', value: '999' },
     ],
-    rates: { RUB: 80.28351, USD: 1, KZT: 450.187855 } as Rates,
+    rates: {
+      RUB: 80.28351,
+      USD: 1,
+      KZT: 450.187855,
+      EUR: 0.923331,
+    } as Rates,
     userRates: { RUB_USD: ['USD', 'RUB', 82] },
   };
+
   it('Process CHANGE_VALUE action', () => {
-    const actionType = 'CHANGE_VALUE';
-    const index = 1;
     const newState = reducer(initialState, {
-      type: actionType,
-      payload: { index, value: '1000,00' },
+      type: 'CHANGE_VALUE',
+      payload: { index: 1, value: '1000,00' },
     });
     expect(newState.currencyIO[0].value).toBe('5607.48');
     expect(newState.currencyIO[1].value).toBe('1000.00');
     expect(newState.currencyIO[2].value).toBe('12.2');
+  });
+
+  it('Process CHANGE_CURRENCY action', () => {
+    // Without user rate
+    let newState = reducer(initialState, {
+      type: 'CHANGE_CURRENCY',
+      payload: {
+        index: 0,
+        currency: 'EUR',
+      },
+    });
+    expect(newState.currencyIO[0].value).toBe('2.05');
+
+    // With user rate
+    newState = reducer(initialState, {
+      type: 'CHANGE_CURRENCY',
+      payload: {
+        index: 1,
+        currency: 'USD',
+      },
+    });
+    expect(newState.currencyIO[1].value).toBe('12.18');
   });
 });
--- src/pages/Converter/controller/reducer.ts
+++ src/pages/Converter/controller/reducer.ts
@@ -10,6 +10,8 @@ import {
   createStateCopy,
 } from '../../../lib/helpers';
 import { recalculateCurrencies } from './dataProcessing';
+import { getRate } from './dataProcessing';
+import { recalculateCurrencyBlock } from './dataProcessing';
 
 interface ChangeValueAction {
   type: 'CHANGE_VALUE';
@@ -61,19 +63,29 @@ export function reducer(
   const { type, payload } = action;
 
   if (type === 'CHANGE_VALUE') {
+    // Extract data
     const { index, value } = payload;
-    // Clear user input
+
+    // Clear user input and change corresponding currency block value
     const clearedInput = clearNumericInput(value);
-    // Change current value in new state
     newState.currencyIO[index].value = clearedInput;
-    // Recalculate currency IO blocks
+
+    // Recalculate all currency IO blocks
     recalculateCurrencies(newState, index);
   }
 
   if (type === 'CHANGE_CURRENCY') {
+    // Extract data
     const { index, currency: to } = payload;
-    const { currency: from, value } = state.currencyIO[index];
-    // Recalculate current block
+    const currencyBlock = newState.currencyIO[index];
+    const { currency: from, value } = currencyBlock;
+
+    // Get rate and change currency of corresponding currency block
+    const rate = getRate(from, to, newState);
+    currencyBlock.currency = to;
+
+    // Recalculate corresponding currency block
+    recalculateCurrencyBlock(currencyBlock, value, rate);
   }
 
   return newState;
