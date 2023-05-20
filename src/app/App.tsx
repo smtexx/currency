c6266eefcdf54fed784cd7774dd0f4359508bc8b
Roman Kolmakov <smtexx@hotmail.com>
2023-05-20 14:56:54 +0200

    fix: move Converter from pages to app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import Layout from '../layout/Layout';
-import Converter from '../pages/Converter/view/Converter';
+import Converter from './Converter/view/Converter';
 import './styles/index.scss';
 
 export default function App() {
