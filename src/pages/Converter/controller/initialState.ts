c70e3109a995e84e73ff43d5c794075d71785ddd
Roman Kolmakov <smtexx@hotmail.com>
2023-05-20 10:48:08 +0200

    feat: change quantity of currencyIO blocks

--- src/pages/Converter/controller/initialState.ts
+++ src/pages/Converter/controller/initialState.ts
@@ -1,11 +1,12 @@
-import { ConverterState } from '../../../types';
+import { ConverterState, ConverterStatus } from '../../../types';
 
 export const initialState: ConverterState = {
   updated: null,
-  status: 'UPDATING',
+  status: ConverterStatus.UPDATING,
   currencyIO: [
     { currency: 'EUR', value: '1' },
     { currency: 'RSD', value: '117.5' },
+    { currency: 'RUB', value: '88.4' },
   ],
   rates: null,
   userRates: {},
