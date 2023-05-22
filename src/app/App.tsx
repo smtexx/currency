d4531eefd285b578aee6be32e474d47e0cf48b53
Roman Kolmakov <smtexx@hotmail.com>
2023-05-22 17:02:00 +0200

    feat: connect converter context to app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,18 @@
+import { useReducer } from 'react';
 import Layout from '../layout/Layout';
 import Converter from './Converter/view/Converter';
+import { reducer } from './Converter/controller/reducer';
+import { initialState } from './Converter/controller/initialState';
+import { ConverterContext } from './Converter/controller/context';
 import './styles/index.scss';
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <Layout>
-      <Converter />
-    </Layout>
+    <ConverterContext.Provider value={{ state, dispatch }}>
+      <Layout>
+        <Converter />
+      </Layout>
+    </ConverterContext.Provider>
   );
 }
