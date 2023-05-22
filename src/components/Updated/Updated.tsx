940d30996cd0732d76f54151dbb0bc08f791faf7
Roman Kolmakov <smtexx@hotmail.com>
2023-05-22 17:57:33 +0200

    feat: connect state to component

--- src/components/Updated/Updated.tsx
+++ src/components/Updated/Updated.tsx
@@ -1,17 +1,73 @@
-import { RiExchangeDollarFill } from 'react-icons/ri';
+import {
+  RiExchangeDollarFill,
+  RiAlertLine,
+  RiLoader4Line,
+} from 'react-icons/ri';
 import s from './Updated.module.scss';
 import HelpButton from '../HelpButton/HelpButton';
+import { useContext } from 'react';
+import { ConverterContext } from '../../app/Converter/controller/context';
+import { ConverterStatus } from '../../types';
+import { setClass } from '../../lib/helpers';
 
 export default function Updated() {
-  return (
-    <p className={s.wrapper}>
-      <span className={s.icon}>
-        <RiExchangeDollarFill />
-      </span>{' '}
-      <span className={s.text}>Обновлено: 14.04 - 02:45</span>
-      <span className={s.button}>
-        <HelpButton />
-      </span>
-    </p>
-  );
+  const { state } = useContext(ConverterContext);
+
+  if (
+    state.status === ConverterStatus.READY &&
+    typeof state.updated === 'number'
+  ) {
+    const lastUpdate = new Date(state.updated);
+    return (
+      <p className={s.wrapper}>
+        <span className={s.icon}>
+          <RiExchangeDollarFill />
+        </span>{' '}
+        <span className={s.text}>
+          Обновлено:{' '}
+          {`${lastUpdate
+            .toLocaleString('ru-RU', {
+              month: '2-digit',
+              day: 'numeric',
+              hour: '2-digit',
+              minute: '2-digit',
+            })
+            .replace(',', ' -')}`}
+        </span>
+        <span className={s.button}>
+          <HelpButton />
+        </span>
+      </p>
+    );
+  }
+
+  if (state.status === ConverterStatus.ERROR) {
+    return (
+      <p className={setClass([[s.wrapper], [s.error]])}>
+        <span className={s.icon}>
+          <RiAlertLine />
+        </span>{' '}
+        <span className={s.text}>Ошибка обновления</span>
+        <span className={s.button}>
+          <HelpButton />
+        </span>
+      </p>
+    );
+  }
+
+  if (state.status === ConverterStatus.UPDATING) {
+    return (
+      <p className={s.wrapper}>
+        <span className={setClass([[s.icon], [s.rotate]])}>
+          <RiLoader4Line />
+        </span>{' '}
+        <span className={s.text}>Обновление курсов</span>
+        <span className={s.button}>
+          <HelpButton />
+        </span>
+      </p>
+    );
+  }
+
+  return null;
 }
