01295a5b7f970394ddaac444874a787b9e94c1aa
Roman Kolmakov <smtexx@hotmail.com>
2023-05-23 16:18:14 +0200

    fix: unblock scroll after app is loaded

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,27 +3,21 @@ import s from './Loader.module.scss';
 import { useEffect, useState } from 'react';
 
 export default function Loader() {
-  useEffect(() => {
-    document.body.style.overflowY = 'hidden';
-
-    return () => {
-      document.body.style.overflowY = '';
-    };
-  });
-
   // Hide loader if app is ready
   const [shown, setShown] = useState(true);
   function hideLoader() {
     setShown(false);
+    document.body.style.overflowY = '';
   }
   useEffect(() => {
     if (document.readyState === 'complete') {
       hideLoader();
     } else {
+      document.body.style.overflowY = 'hidden';
       window.addEventListener('load', hideLoader);
       return () => window.removeEventListener('load', hideLoader);
     }
-  });
+  }, []);
 
   return shown ? (
     <div className={s.wrapper}>
