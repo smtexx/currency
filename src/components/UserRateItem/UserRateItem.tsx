37d71c1bd46ab7207dd6ada01a628a914dbecd1e
Roman Kolmakov <smtexx@hotmail.com>
2023-05-27 11:07:25 +0200

    feat: connect to state

--- src/components/UserRateItem/UserRateItem.tsx
+++ src/components/UserRateItem/UserRateItem.tsx
@@ -2,20 +2,44 @@ import { BiChevronsRight } from 'react-icons/bi';
 import { IoCloseSharp } from 'react-icons/io5';
 import s from './UserRateItem.module.scss';
 import { setClass } from '../../lib/helpers';
+import { Currency } from '../../types';
+import { ConverterContext } from '../../app/Converter/controller/context';
+import { useContext } from 'react';
 
-export default function UserRateItem() {
+interface I_Props {
+  from: Currency;
+  to: Currency;
+  value: string;
+}
+
+export default function UserRateItem({ from, to, value }: I_Props) {
+  const { dispatch } = useContext(ConverterContext);
+
+  function removeRate() {
+    dispatch({
+      type: 'REMOVE_USER_RATE',
+      payload: {
+        from,
+        to,
+      },
+    });
+  }
+
   return (
     <li className={s.wrapper}>
-      <span className={s.currency}>EUR</span>
+      <span className={s.currency}>{from}</span>
       <span className={s.arrow}>
         <BiChevronsRight />
       </span>
-      <span className={s.currency}>RSD</span>
+      <span className={s.currency}>{to}</span>
       <span className={s.arrow}>
         <BiChevronsRight />
       </span>
-      <span className={s.value}>117.5</span>
-      <button className={setClass([['cm-link-hover'], [s.close]])}>
+      <span className={s.value}>{value}</span>
+      <button
+        className={setClass([['cm-link-hover'], [s.close]])}
+        onClick={removeRate}
+      >
         <IoCloseSharp />
       </button>
     </li>
--- src/components/UserRatesAdded/UserRatesAdded.tsx
+++ src/components/UserRatesAdded/UserRatesAdded.tsx
@@ -1,15 +1,30 @@
+import { ConverterContext } from '../../app/Converter/controller/context';
 import { setClass } from '../../lib/helpers';
+import { Currency } from '../../types';
 import UserRateItem from '../UserRateItem/UserRateItem';
 import s from './UserRatesAdded.module.scss';
+import { useContext } from 'react';
 
 export default function UserRatesAdded() {
+  const { state } = useContext(ConverterContext);
+  const userRates = Object.entries(state.userRates).map(
+    (entry) => entry.flat() as [string, Currency, Currency, number]
+  );
+
   return (
     <div className={setClass([['cm-inner-block'], [s.wrapper]])}>
       <h3 className="cm-inner-block-header">Сохраненные курсы</h3>
       <ul className={s.ratesHolder}>
-        <UserRateItem />
-        <UserRateItem />
-        <UserRateItem />
+        {userRates.length === 0
+          ? 'Пользовательские курсы отсутствуют'
+          : userRates.map(([id, from, to, value]) => (
+              <UserRateItem
+                from={from}
+                to={to}
+                value={value.toString()}
+                key={id}
+              />
+            ))}
       </ul>
     </div>
   );
