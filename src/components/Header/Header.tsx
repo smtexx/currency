dc933351bd8d8af0827da21ee702fe201d7d1582
Roman Kolmakov <smtexx@hotmail.com>
2023-05-28 15:03:27 +0200

    content: change main header text

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,17 @@
 import Updated from '../Updated/Updated';
+import { SiExpertsexchange } from 'react-icons/si';
 import s from './Header.module.scss';
+
 export default function Header() {
   return (
     <header className={s.wrapper}>
-      <h1 className={s.header}>Currency Converter</h1>
+      <div className={s.header}>
+        <span className={s.icon}>
+          <SiExpertsexchange />
+        </span>
+        <h1 className={s.headering}>XChange_PRO</h1>
+      </div>
+      <p className={s.description}>Конвертер валют</p>
       <div className={s.updated}>
         <Updated />
       </div>
