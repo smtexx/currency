11d058ec71c3cb91420ece61a1818cb0d0e4e869
Roman Kolmakov <smtexx@hotmail.com>
2023-06-01 14:15:15 +0200

    init: create component and styles

---
+++ src/components/Settings/Settings.tsx
@@ -0,0 +1,93 @@
+import { RiSettings5Line } from 'react-icons/ri';
+import s from './Settings.module.scss';
+import Dialog from '../Dialog/Dialog';
+import { useState } from 'react';
+import { createStateCopy, setClass } from '../../lib/helpers';
+import { I_SettingsSection, config } from './config';
+
+export default function Settings() {
+  const [open, setOpen] = useState(false);
+  const [settings, setSettings] = useState(
+    config.map((section) => section.fields)
+  );
+
+  function changeSettings(
+    fieldsetIdx: number,
+    fieldValue: string,
+    handler: I_SettingsSection['handler']
+  ) {
+    const newSettings = createStateCopy(settings);
+    newSettings[fieldsetIdx] = handler(
+      fieldValue,
+      newSettings[fieldsetIdx]
+    );
+    setSettings(newSettings);
+  }
+
+  return (
+    <>
+      <button
+        className="cm-icon-button cm-link-hover"
+        onClick={() => setOpen(true)}
+      >
+        <RiSettings5Line />
+      </button>
+      <Dialog open={open}>
+        <header className="cm-dialog-header">
+          <h2>Настройки</h2>
+        </header>
+        <div
+          className={setClass([['cm-dialog-content'], [s.content]])}
+        >
+          <p className={s.tip}>
+            Здесь вы можете изменять различные настройки, а также
+            управлять сохранением данных приложения.
+          </p>
+          <form className={s.settings}>
+            {config.map((fieldset, fieldsetIdx) => (
+              <fieldset key={fieldsetIdx}>
+                <legend>{fieldset.legend}</legend>
+                {fieldset.fields.map((field, fieldIdx) => {
+                  const ID = `settings_field_${field.value}`;
+                  const { checked, value } =
+                    settings[fieldsetIdx][fieldIdx];
+                  return (
+                    <label htmlFor={ID} key={field.value}>
+                      {field.label}
+                      <input
+                        type="checkbox"
+                        className="cm-checkbox"
+                        id={ID}
+                        checked={checked}
+                        onChange={() =>
+                          changeSettings(
+                            fieldsetIdx,
+                            value,
+                            fieldset.handler
+                          )
+                        }
+                      />
+                    </label>
+                  );
+                })}
+              </fieldset>
+            ))}
+          </form>
+
+          <button className="cm-text-button">
+            Установить как приложение
+          </button>
+        </div>
+        <div className="cm-dialog-controls">
+          <button
+            className="cm-text-button"
+            onClick={() => setOpen(false)}
+          >
+            Отмена
+          </button>
+          <button className="cm-text-button">Сохранить</button>
+        </div>
+      </Dialog>
+    </>
+  );
+}
---
+++ src/components/Settings/config.tsx
@@ -0,0 +1,106 @@
+interface I_Field {
+  label: string;
+  value: string;
+  checked: boolean;
+}
+
+export interface I_SettingsSection {
+  legend: string;
+  fields: I_Field[];
+  handler: (
+    fieldValue: I_Field['value'],
+    fields: I_Field[]
+  ) => I_Field[];
+}
+
+type T_Settings = I_SettingsSection[];
+
+export const config: T_Settings = [
+  {
+    legend: 'Язык интерфейса',
+    fields: [
+      { label: 'Русский', value: 'russian', checked: true },
+      { label: 'English', value: 'english', checked: false },
+    ],
+    handler(fieldValue, fields) {
+      fields.forEach((field) => {
+        field.checked = field.value === fieldValue;
+      });
+
+      return fields;
+    },
+  },
+  {
+    legend: 'Конвертация',
+    fields: [
+      {
+        label: 'Дополнительный блок',
+        value: 'additionalBlock',
+        checked: false,
+      },
+    ],
+    handler(fieldValue, fields) {
+      fields.forEach((field) => {
+        if (field.value === fieldValue) {
+          field.checked = !field.checked;
+        }
+      });
+
+      return fields;
+    },
+  },
+  {
+    legend: 'Сохранение',
+    fields: [
+      { label: 'Курсы валют', value: 'saveRates', checked: false },
+      {
+        label: 'Добавленные курсы',
+        value: 'saveUserRates',
+        checked: true,
+      },
+      {
+        label: 'Введенные значения',
+        value: 'saveExchangeData',
+        checked: true,
+      },
+      {
+        label: 'Настройки приложения',
+        value: 'saveAppSettings',
+        checked: true,
+      },
+      {
+        label: 'Удалить все данные',
+        value: 'clearAllData',
+        checked: false,
+      },
+    ],
+    handler(fieldValue, fields) {
+      const clearFieldValue = 'clearAllData';
+      const clearFieldChecked =
+        fields.find(({ value }) => value === clearFieldValue)
+          ?.checked || false;
+
+      if (fieldValue === clearFieldValue && !clearFieldChecked) {
+        // Switch off other fields if switch on clearAllData
+        fields.forEach((field) => {
+          if (field.value === clearFieldValue) {
+            field.checked = true;
+          } else {
+            field.checked = false;
+          }
+        });
+      } else {
+        fields.forEach((field) => {
+          if (field.value === clearFieldValue) {
+            field.checked = false;
+          }
+          if (field.value === fieldValue) {
+            field.checked = !field.checked;
+          }
+        });
+      }
+
+      return fields;
+    },
+  },
+];
